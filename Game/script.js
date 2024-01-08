c453697f7336a6d6bfb43687d3e76169c0069ff3
Oliver Santos <106833596+RyZe-00@users.noreply.github.com>
2024-01-08 00:26:46 -0500

    Add files via upload

---
+++ Game/script.js
@@ -0,0 +1,165 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const canvas = document.getElementById('snakeCanvas');
+    const ctx = canvas.getContext('2d');
+
+    const gridSize = 20;
+    const canvasSize = 400;
+    const snake = [{ x: 0, y: 0 }];
+    let score = 0;
+    let direction = 'right';
+    let food = generateFood();
+    let gameEnded = false;
+
+    //Puntaje
+    function updateScore() {
+        const scoreElement = document.getElementById('score');
+        scoreElement.textContent = `Puntaje: ${score}`;
+    }
+    
+    //FIN DEL JUEGO
+    function endGame() {
+        const finalScore = score;
+        
+        Swal.fire({
+            icon: 'error',
+            title: 'Game Over',
+            text: `Puntaje obtenido: ${finalScore}`,
+            showCancelButton: true,
+            confirmButtonText: 'Reintentar',
+            cancelButtonText: 'Salir',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                resetGame();
+            } else {
+                // Aquí puedes redirigir al usuario o hacer cualquier otra acción al salir del juego
+                window.location.href = '../index.html';
+            }
+        });
+        
+    }
+    
+    //GENERAR LA COMIDA
+    function generateFood() {
+        let newFood;
+        do {
+            newFood = {
+                x: Math.floor(Math.random() * (canvasSize / gridSize)) * gridSize,
+                y: Math.floor(Math.random() * (canvasSize / gridSize)) * gridSize
+            };
+        } while (snake.some(segment => segment.x === newFood.x && segment.y === newFood.y));
+        
+        return newFood;
+    }
+
+    function draw() {
+        // Limpiar el canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Dibujar el fondo blanco del tablero
+        ctx.fillStyle = '#FFFFFF'; // Color blanco
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Dibujar los límites del área de juego
+        ctx.strokeStyle = '#000000'; // Color negro
+        ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+        // Dibujar la serpiente
+        ctx.fillStyle = '#4CAF50';
+        snake.forEach(segment => ctx.fillRect(segment.x, segment.y, gridSize, gridSize));
+
+        // Dibujar la comida
+        ctx.fillStyle = '#FF0000';
+        ctx.fillRect(food.x, food.y, gridSize, gridSize);
+    }
+
+    function move() {
+        const head = Object.assign({}, snake[0]);
+
+        switch (direction) {
+            case 'up':
+                head.y -= gridSize;
+                break;
+            case 'down':
+                head.y += gridSize;
+                break;
+            case 'left':
+                head.x -= gridSize;
+                break;
+            case 'right':
+                head.x += gridSize;
+                break;
+        }
+
+        // Verificar colisión con la comida
+        if (head.x === food.x && head.y === food.y) {
+            snake.unshift(food);
+            food = generateFood();
+            score += 10; 
+            updateScore();
+        } else {
+            snake.pop();
+            snake.unshift(head);
+        }
+        
+        // Verificar colisión con las paredes
+        if (head.x < 0 || head.x >= canvas.width || head.y < 0 || head.y >= canvas.height) {
+            endGame();
+            resetGame();
+        }
+
+        // Verificar colisión con la propia serpiente
+        for (let i = 1; i < snake.length; i++) {
+            if (head.x === snake[i].x && head.y === snake[i].y) {
+                endGame();
+                resetGame();
+            }
+        }
+    }
+
+    function resetGame() {
+        snake.length = 0;
+        snake.push({ x: 0, y: 0 }); // Agrega la posición inicial de la serpiente
+        direction = 'right';
+        food = generateFood();
+        score = 0;
+        updateScore();
+
+        gameEnded = false;
+    }
+
+    function gameLoop() {
+        if (!gameEnded) {
+            move();
+            draw();
+            setTimeout(gameLoop, 100);
+        }
+    }
+
+    document.addEventListener('keydown', (event) => {
+        switch (event.key) {
+            case 'ArrowUp':
+                if (direction !== 'down') {
+                    direction = 'up';
+                }
+                break;
+            case 'ArrowDown':
+                if (direction !== 'up') {
+                    direction = 'down';
+                }
+                break;
+            case 'ArrowLeft':
+                if (direction !== 'right') {
+                    direction = 'left';
+                }
+                break;
+            case 'ArrowRight':
+                if (direction !== 'left') {
+                    direction = 'right';
+                }
+                break;
+        }
+    });
+
+    gameLoop();
+});
+
--- script.js
+++ script.js
@@ -1,98 +1,144 @@
 //Facebook
-window.fbAsyncInit = function() {
+window.fbAsyncInit = function () {
   FB.init({
-    appId      : '380895757815230',
-    cookie     : true,
-    xfbml      : true,
-    version    : 'v18.0'
+    appId: "380895757815230",
+    cookie: true,
+    xfbml: true,
+    version: "v18.0",
   });
-    
-  FB.AppEvents.logPageView(); 
-    
+
+  FB.AppEvents.logPageView();
 };
 
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "https://connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+(function (d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, "script", "facebook-jssdk");
 
- 
-FB.getLoginStatus(function(response) {
+FB.getLoginStatus(function (response) {
   statusChangeCallback(response);
-  if (response.status === 'connected') {
+  if (response.status === "connected") {
     console.log(response.authResponse.accessToken);
   }
 });
 
-
 function checkLoginState() {
-  FB.getLoginStatus(function(response) {
+  FB.getLoginStatus(function (response) {
     statusChangeCallback(response);
-    if (response.status === 'connected') {
+    if (response.status === "connected") {
       console.log(response.authResponse.accessToken);
     }
   });
 }
 
 function customFacebookLogin() {
-  FB.login(function(response) {
+  FB.login(function (response) {
     if (response.authResponse) {
-         console.log('Bienvenido!  Gracias por tu informacion.... ');
-         FB.api('/me', {fields: 'name, email'}, function(response) {
-          if (response.email) {
-            alert("'Bienvenido!  Gracias por tu informacion.... ' \nTu nombre es, " + response.name + ". \nTu email es " + response.email);
-          } else {
-            alert("'Bienvenido!  Gracias por tu informacion.... ' \nTu nombre es, " + response.name + ". \nTu email es " + response.email);
-            console.log('Correo electrónico no disponible');
-          }
-         });
-         
-    } else { 
-         console.log('Inicio de Sesion Cancelado'); }
+      console.log("Bienvenido!  Gracias por tu informacion.... ");
+      FB.api("/me", { fields: "name, email" }, function (response) {
+        if (response.email) {
+          alert(
+            "'Bienvenido!  Gracias por tu informacion.... ' \nTu nombre es, " +
+              response.name +
+              ". \nTu email es " +
+              response.email
+          );
+        } else {
+          alert(
+            "'Bienvenido!  Gracias por tu informacion.... ' \nTu nombre es, " +
+              response.name +
+              ". \nTu email es " +
+              response.email
+          );
+          console.log("Correo electrónico no disponible");
+        }
+      });
+    } else {
+      console.log("Inicio de Sesion Cancelado");
+    }
   });
 }
 
-FB.api('/me', function(response) {
+FB.api("/me", function (response) {
   console.log(JSON.stringify(response));
 });
-
-
 
 //GOOGLE
 function handleCredentialResponse(response) {
   if (response.credential) {
     var credential = response.credential;
 
-    // Muestra la información en una ventana emergente
-    alert("ID de usuario: " + credential.id + "\nNombre: " + credential.name + "\nEmail: " + credential.email);
-
-    // Abre una nueva ventana con la información del usuario
-    var userInfoWindow = window.open('', '_blank');
-    userInfoWindow.document.write('<html><body>');
-    userInfoWindow.document.write('<p>ID de usuario: ' + credential.id + '</p>');
-    userInfoWindow.document.write('<p>Nombre: ' + credential.name + '</p>');
-    userInfoWindow.document.write('<p>Email: ' + credential.email + '</p>');
-    userInfoWindow.document.write('</body></html>');
-    userInfoWindow.document.close();
+    Swal.fire({
+      icon: "info",
+      title: "Información de Usuario",
+      html: `<p>ID de usuario: ${credential.id}</p>
+             <p>Nombre: ${credential.name}</p>
+             <p>Email: ${credential.email}</p>`,
+    }).then((result) => {
+      // Puedes realizar acciones adicionales después de que el usuario interactúe con la ventana emergente
+    });
 
     // Maneja la información de la credencial como desees
     console.log("ID de usuario:", credential.id);
     console.log("Nombre:", credential.name);
     console.log("Email:", credential.email);
 
-    window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0&ab_channel=Duran'
+    window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0&ab_channel=Duran";
   }
 }
 
 // Inicializa Google Identity Services y configura la función de devolución de llamada
 google.accounts.id.initialize({
-  client_id: '156922708305-2s4kunvt2aatctbfkpt00pgliqpojkuh.apps.googleusercontent.com',
+  client_id:
+    "156922708305-2s4kunvt2aatctbfkpt00pgliqpojkuh.apps.googleusercontent.com",
   callback: handleCredentialResponse,
   cancel_on_tap_outside: false,
 });
 
 // Renderiza el botón de Inicio de Sesión de Google
-google.accounts.id.renderButton(document.querySelector('.g_id_signin'));
+google.accounts.id.renderButton(document.querySelector(".g_id_signin"));
+
+//LOCAL
+function iniciarSesionLocal() {
+  // Capturamos valores de los campos de entrada
+  const correo = document.getElementById("correo").value;
+  const password = document.getElementById("password").value;
+
+  // Simulamos la autenticación local (sin comunicación con el servidor en este ejemplo)
+  if (correo === "usuario@ejemplo.com" && password === "contraseña") {
+    Swal.fire({
+      icon: "success",
+      title: "Inicio de Sesión Exitoso",
+      text: "Bienvenido",
+    }).then((result) => {
+      window.location.href = 'Game/snake.html';
+      // Aquí puedes redirigir al usuario a otra página
+      //window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0&ab_channel=Duran'
+    });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Credenciales incorrectas",
+    }).then((result) => {
+      // Aquí puedes redirigir al usuario a otra página después de un inicio de sesión incorrecto
+      window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0&ab_channel=Duran';
+    });
+
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const botonIniciarSesion = document.querySelector(".input-submit");
+  botonIniciarSesion.addEventListener("click", iniciarSesionLocal);
+});
+
+//EASTER EGG
+
