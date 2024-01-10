7189f05077001681787e88539ea0e092ff40fa4b
Oliver Santos <106833596+RyZe-00@users.noreply.github.com>
2024-01-10 13:46:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -76,6 +76,15 @@ FB.api("/me", function (response) {
 
 //GOOGLE
 function handleCredentialResponse(response) {
+  const responsePayload = decodeJwtResponse(response.credential);
+
+  console.log("ID: " + responsePayload.sub);
+  console.log('Nombre completo: ' + responsePayload.name);
+  console.log('Nombre dado: ' + responsePayload.given_name);
+  console.log('Apellido: ' + responsePayload.family_name);
+  console.log("URL de la imagen: " + responsePayload.picture);
+  console.log("Correo electrónico: " + responsePayload.email);
+  
   if (response.credential) {
     var credential = response.credential;
 
